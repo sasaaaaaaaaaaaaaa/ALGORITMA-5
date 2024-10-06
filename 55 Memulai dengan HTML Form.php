562a1009991972879bbc4a510cf6daa7e40e1d93
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-07 06:33:51 +0700

    Memulai dengan HTML Form

---
+++ 55 Memulai dengan HTML Form.php
@@ -0,0 +1,12 @@
+<form method="post" action="sendmail.php">
+    <label for="name">Nama:</label>
+    <input type="text" id="name" name="name">
+    
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email">
+    
+    <label for="message">Pesan:</label>
+    <textarea id="message" name="message"></textarea>
+    
+    <input type="submit" value="Kirim">
+</form>
