c68a7d6e5fbb106f994c963f06767cd8e8a68bcb
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-07 06:23:43 +0700

    Membuat Form HTML

---
+++ 50 Membuat Form HTML.php
@@ -0,0 +1,5 @@
+<form method="post" action="proses_form.php">
+  Nama: <input type="text" name="nama" required>
+  Email: <input type="email" name="email" required>
+  <input type="submit" value="Kirim">
+</form>
