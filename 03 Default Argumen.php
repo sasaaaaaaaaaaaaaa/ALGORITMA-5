5c61452425cdd5427f935bf41e25faafc6f0f0f1
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-06 15:21:05 +0700

    default argumen

---
+++ 03 Default Argumen.php
@@ -0,0 +1,7 @@
+<?php
+function nama_bulan($bulan, $tahun = 2016) {
+	echo $bulan . ' ' . $tahun;
+}
+nama_bulan('Desember'); // Hasil Januari 2016
+echo"<hr>";
+nama_bulan('alika'); // Hasil Januari 2016
