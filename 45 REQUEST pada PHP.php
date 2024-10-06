db4bee30e908e3c128be3ef47639cee371696a41
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-07 05:54:14 +0700

    REQUEST pada PHP

--- 45 REQUEST pada PHP.php
+++ 45 REQUEST pada PHP.php
@@ -3,6 +3,7 @@
 	<form method="POST" action="?action=edit">
 		<input type="text" name="nama"><br>
 		<input type="text" name="email"><br>
+		<input type="hidden" name="action" value="insert">
 		<input type="submit" name="submit" value="submit">
 	</form>
 
@@ -12,8 +13,8 @@
 		echo '<pre>'; 
 		print_r($_GET);
 		print_r($_POST);
+		print_r($_REQUEST);
 	}
-  ?>
+	?>
 </body>
 </html>
-
