b729984ad097c140ac25265096537a06f09a0527
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-07 05:18:06 +0700

    Penggunaan Fungsi empty

---
+++ 37 Penggunaan Fungsi empty.php
@@ -0,0 +1,21 @@
+<?php
+$var1 = "";
+$var2 = 0;
+$var3 = false;
+$var4 = array();
+
+if(empty($var1)) {
+    echo '$var1 kosong';
+}
+
+if(empty($var2)) {
+    echo '$var2 kosong';
+}
+
+if(empty($var3)) {
+    echo '$var3 kosong';
+}
+
+if(empty($var4)) {
+    echo '$var4 kosong';
+}
