1fcc7fc23f27bab9e63f9cb162b4b124018db42e
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-07 06:18:38 +0700

    Membuat Formulir Sederhana

---
+++ 48 Membuat Formulir Sederhana.php
@@ -0,0 +1,9 @@
+<form action="handle-post.php" method="post">
+  <label for="name">Nama:</label>
+  <input type="text" id="name" name="name">
+  
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email">
+  
+  <input type="submit" value="Kirim">
+</form>
