bbcc92415207da4a4c6a95a2ce023057bd1d92c2
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-07 05:45:29 +0700

    Method POST

--- 43 Method POST.php
+++ 43 Method POST.php
@@ -1,18 +1,23 @@
 <html lang="en">
  
 <head>
-    <title>Method GET</title>
+    <title>Method POST</title>
 </head>
  
 <body>
-    <form action="" method="GET">
+    <form action="" method="POST">
         <input type="text" name="nama"><br />
         <input type="number" name="umur"><br />
         <input type="submit" name="submit" value="Sumbit">
     </form>
  
     <?php
-    if ($_GET) {
-        <form method="GET" action="">
-		<input type="text" name="nama"><br>
-		<input type="text" name="email"><br>
+    if ($_POST) {
+        echo "Nama: " . $_POST["nama"];
+        echo "<br/>";
+        echo "Umur: " . $_POST["umur"];
+    }
+    ?>
+</body>
+ 
+</html>
