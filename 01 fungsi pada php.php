8c286dad01726bc8fbeafd611821cf760a4655f1
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-10-06 15:12:37 +0700

    fungsi pekenalan

--- 01 fungsi pada php.php
+++ 01 fungsi pada php.php
@@ -2,7 +2,7 @@
 // mmbuat fungsi
 function perkenalan(){
   echo "Assalamulaikum, ";
-  echo "Perkenalkan, nama kita Alika<br/>";
+  echo "Perkenalkan, nama kita Ahmadi<br/>";
   echo "Senang berkenalan dengan anda<br/>";
 }
 // memanggil fungsi yang sudah dibuat
